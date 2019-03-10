2b2963094152e2038d79b47ee7226e160e53e81f
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2019-03-10 18:21:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,132 +2,134 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "$";
 
+client.on('message',async function(message) {
+  if(message.author.bot) return;
+  if(message.channel.type === 'dm') return;
 
-var cooldown = new Set();
-var points = {};
-client.on('message', async message => {
-	if(message.channel.type !== 'text') return;
-	
-	
-	var command = message.content.toLowerCase().split(" ")[0];
-	var args = message.content.toLowerCase().split(" ");
-	var userM = message.guild.member(message.mentions.users.first() || message.guild.members.find(m => m.id == args[1]));
-	  const embed  = new Discord.RichEmbed()
-.setDescription(`
-**لم يتم تسجيل أي نقطة حتى الأن **
-** أمثلة للأوامر: **
-**:small_orange_diamond:** $points ${message.author} 1 \`لتغيير نقاط شخص معين \`
-**:small_orange_diamond:** $points ${message.author} +1 \`لزيادة نقاط شخص معين\`
-**:small_orange_diamond:** $points ${message.author} -1 \`لأزالة نقطة من شخص معين \`
-**:small_orange_diamond:** $points ${message.author} 0 \`لتصفير نقاط شخص معين \`
-**:small_orange_diamond:** $points reset \`لتصفير جميع النقاط\``)
-.setFooter('Requested by '+message.author.username, message.author.avatarURL)
-.setColor(`#e60909`)
-  const error  = new Discord.RichEmbed()
-.setDescription(`
-**:x: | يجب كتابة الأمر بشكل صحيح. **
-** أمثلة للأوامر: **
-**:small_orange_diamond:** $points ${message.author} 1 \`لتغيير نقاط شخص معين \`
-**:small_orange_diamond:** $points ${message.author} +1 \`لزيادة نقاط شخص معين\`
-**:small_orange_diamond:** $points ${message.author} -1 \`لأزالة نقطة من شخص معين \`
-**:small_orange_diamond:** $points ${message.author} 0 \`لتصفير نقاط شخص معين \`
-**:small_orange_diamond:** $points reset \`لتصفير جميع النقاط\``)
-.setFooter('Requested by '+message.author.username, message.author.avatarURL)
-.setColor(`#e60909`)
-if(command == prefix + 'points') {
-	 
-		if(!message.guild.member(client.user).hasPermission('EMBED_LINKS')) return message.channel.send(':no_entry: | I dont have Embed Links permission.');
-		if(!args[1]) {
-			if(!points) return message.channel.send(embed);
-			var members = Object.values(points);
-			var memb = members.filter(m => m.points >= 1);
-			if(memb.length == 0) return message.channel.send(embed);
-			var x = 1;
-			let pointsTop = new Discord.RichEmbed()
-			.setAuthor('Points:')
-			.setColor('#FBFBFB')
-			.setDescription(memb.sort((second, first) => first.points > second.points).slice(0, 10).map(m => `**:small_blue_diamond:** <@${m.id}> \`${m.points}\``).join('\n'))
-			.setFooter(`Requested by ${message.author.username}`, message.author.avatarURL);
-			message.channel.send({
-				embed: pointsTop
-			});
-		}else if(args[1] == 'reset') {
-			let pointsReset = new Discord.RichEmbed()
-			.setDescription('**:white_check_mark: | تم تصفير جميع النقاظ بنجاح**')
-			.setFooter('Requested by '+message.author.username, message.author.avatarURL)
-			if(!message.member.hasPermission('MANAGE_GUILD')) return err(message, "You dont have Manage Server permission.");
-			if(!points) return message.channel.send(pointsReset);
-			var members = Object.values(points);
-			var memb = members.filter(m => m.points >= 1);
-			if(memb.length == 0) return message.channel.send(pointsReset);
-			points = {};
-			message.channel.send(pointsReset);
-		}else if(userM) {
-			if(!message.member.hasPermission('MANAGE_GUILD')) return err(message, "You dont have Manage Server permission.");
-			if(!points[userM.user.id]) points[userM.user.id] = {
-				points: 0,
-				id: userM.user.id
-			};
-			if(!args[2]) {
-				if(points[userM.user.id].points == 0) return err(message, `${userM.user.username} Not have any points.`);
-				var userPoints = new Discord.RichEmbed()
-				.setColor('#d3c325')
-				.setAuthor(`${userM.user.username} have ${points[userM.user.id].points} points.`);
-				message.channel.send({
-					embed: userPoints
-				});
-			}else if(args[2] == 'reset') {
-				if(points[userM.user.id].points == 0) return message.channel.send(error);
-				points[userM.user.id].points = 0;
-				message.channel.send(`Successfully reset ${userM.user.username} points.`);
-			}else if(args[2].startsWith('+')) {
-				args[2] = args[2].slice(1);
-				args[2] = parseInt(Math.floor(args[2]));
-				if(points[userM.user.id].points == 1000000) return message.channel.send(error);
-				if(!args[2]) return message.channel.send(error);
-				if(isNaN(args[2])) return message.channel.send(error);
-				if(args[2] > 1000000) return message.channel.send(error);
-				if(args[2] < 1) return message.channel.send(error);
-				if((points[userM.user.id].points + args[2]) > 1000000) args[2] = 1000000 - points[userM.user.id].points;
-				points[userM.user.id].points += args[2];
-				let add = new Discord.RichEmbed()
-				.setDescription(`**:small_blue_diamond:** <@${userM.id}> \`${points[userM.user.id].points}\``)
-				.setAuthor('Points:')
-				.setColor('#FBFBFB')
-				.setFooter('Requested by' + message.author.username, message.author.avatarURL)
-				message.channel.send(add);
-			}else if(args[2].startsWith('-')) {
-				args[2] = args[2].slice(1);
-				args[2] = parseInt(Math.floor(args[2]));
-				if(points[userM.user.id].points == 0) return message.channel.send(error);
-				if(!args[2]) return message.channel.send(error);
-				if(isNaN(args[2])) return message.channel.send(error);
-				if(args[2] > 1000000) return message.channel.send(error);
-				if(args[2] < 1) return message.channel.send(error);
-				if((points[userM.user.id].points - args[2]) < 0) args[2] = points[userM.user.id].points;
-				points[userM.user.id].points -= args[2];
-					let rem = new Discord.RichEmbed()
-				.setDescription(`**:small_blue_diamond:** <@${userM.id}> \`${points[userM.user.id].points}\``)
-				.setAuthor('Points:')
-				.setColor('#FBFBFB')
-				.setFooter('Requested by' + message.author.username, message.author.avatarURL)
-				message.channel.send(rem);
-			}else if(!args[2].startsWith('+') || !args[2].startsWith('-')) {
-				args[2] = parseInt(Math.floor(args[2]));
-				if(isNaN(args[2])) return message.channel.send(error);
-				if(args[2] > 1000000) return message.channel.send(error);
-				if(args[2] < 1) return message.channel.send(error);
-				if(points[userM.user.id].points == args[2]) return err(message, `${userM.user.username} points is already ${args[2]}.`);
-				points[userM.user.id].points = args[2];
-					let set = new Discord.RichEmbed()
-				.setDescription(`**:small_blue_diamond:** <@${userM.id}> \`${points[userM.user.id].points}\``)
-				.setAuthor('Points:')
-				.setColor('#FBFBFB')
-				.setFooter('Requested by' + message.author.username, message.author.avatarURL)
-				message.channel.send(set);
-			}
-			}
-			}
+  let editRole = message.mentions.roles.first();
+  let args = message.content.split(' ');
+  let color = args[2];
+  let black = '#1c1b1b',
+      white = '#ffffff',
+      lime = '#02e048',
+      blue = '#02def2',
+      purple = '#cb00ff',
+      pink = '#ff0077',
+      red = '#ff0000',
+      orange = '#ffae00',
+      milky = '#005dff',
+      darkgreen = '#2a8942',
+      darkblue = '#2a1151',
+      darkpurple = '#50114b',
+      darkpink = '#99038d',
+      darkred = '#990303',
+      darkorange = '#996103'
+  if(message.content.startsWith(prefix + "setcolor")) {
+    if(!message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return message.channel.send(`**:heavy_multiplication_x: | You dont have permissions**`);
+    if(!message.guild.me.hasPermission('MANAGE_ROLES_OR_PERMISSIONS') || !message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.channel.send(`**:heavy_multiplication_x: | The Bot must have permissions**`);
+    if(!editRole) return message.channel.send(`**:milky_way:| Mention role!**`);
+    if(!args[2]) {
+      let codes = [black, white, lime, blue, purple, pink, red, orange, milky, darkgreen, darkblue, darkpurple, darkpink, darkred, darkorange];
+      let colors = new Discord.RichEmbed()
+      .setTitle(`Role Colors: \`${message.guild.name}\``)
+      .setColor(codes[Math.floor(Math.random() * codes.length)])
+      .setFooter(client.user.username)
+      .setTimestamp()
+.setDescription(`\nBlack: \`${black}\`\n\nWhite: \`${white}\`\n\nLime: \`${lime}\`\n\nBlue: \`${blue}\`\n\nPurple: \`${purple}\`\n\nPink: \`${pink}\`\n\nRed: \`${red}\`\n\nOrange: \`${orange}\`\n\nMilky: \`${milky}\`
+\nDarkGreen: \`${darkgreen}\`\n\nDarkBlue: \`${darkblue}\`\n\nDarkPurple: \`${darkpurple}\`\n\nDarkPink: \`${darkpink}\`\n\nDarkRed: \`${darkred}\`\n\nDarkOrange: \`${darkorange}\`\n`);
+      return message.channel.send(colors);
+    }
+
+    if(args[2] && color === 'white') {
+      editRole.edit({color: white}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      });
+    }
+    if(args[2] && color === 'black') {
+      editRole.edit({color: black}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      });
+    }
+    if(args[2] && color === 'lime') {
+      editRole.edit({color: lime}).then(function(done, err) {
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      }).catch(e => message.channel.send(`**:heavy_multiplication_x: | Error**`));
+    }
+    if(args[2] && color === 'blue') {
+      editRole.edit({color: blue}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      }).catch(e => message.channel.send(`**:heavy_multiplication_x: | Error**`));
+    }
+    if(args[2] && color === 'purple') {
+      editRole.edit({color: purple}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+  if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      }).catch(e => message.channel.send(`**:heavy_multiplication_x: | Error**`));
+    }
+    if(args[2] && color === 'pink') {
+      editRole.edit({color: pink}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      }).catch(e => message.channel.send(`**:heavy_multiplication_x: | Error**`));
+    }
+    if(args[2] && color === 'red') {
+      editRole.edit({color: red}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      }).catch(e => message.channel.send(`**:heavy_multiplication_x: | Error**`));
+    }
+    if(args[2] && color === 'orange') {
+      editRole.edit({color: orange}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      }).catch(e => message.channel.send(`**:heavy_multiplication_x: | Error**`));
+    }
+    if(args[2] && color === 'milky') {
+      editRole.edit({color: milky}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      }).catch(e => message.channel.send(`**:heavy_multiplication_x: | Error**`));
+    }
+    if(args[2] && color === 'darkgreen') {
+      editRole.edit({color: darkgreen}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      }).catch(e => message.channel.send(`**:heavy_multiplication_x: | Error**`));
+    }
+    if(args[2] && color === 'darkblue') {
+      editRole.edit({color: darkblue}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      }).catch(e => message.channel.send(`**:heavy_multiplication_x: | Error**`));
+    }
+    if(args[2] && color === 'darkred') {
+      editRole.edit({color: darkred}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      }).catch(e => message.channel.send(`**:heavy_multiplication_x: | Error**`));
+    }
+    if(args[2] && color === 'darkpink') {
+      editRole.edit({color: darkpink}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      }).catch(e => message.channel.send(`**:heavy_multiplication_x: | Error**`));
+    }
+    if(args[2] && color === 'darkpurple') {
+      editRole.edit({color: darkpurple}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      }).catch(e => message.channel.send(`**:heavy_multiplication_x: | Error**`));
+    }
+    if(args[2] && color === 'darkorange') {
+      editRole.edit({color: darkorange}).then(function(done, err) {
+        if(err) return message.channel.send(`**:heavy_multiplication_x: | Error**`);
+        if(!err) return message.channel.send(`**:ballot_box_with_check: | Done**`);
+      }).catch(e => message.channel.send(`**:heavy_multiplication_x: | Error**`));
+    }
+  }
 });
 
 
